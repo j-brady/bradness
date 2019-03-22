877f73d7053b54f8b9095b6151e470a25eaa9d83
Jacob Brady <jacob.brady0449@gmail.com>
2019-03-22 15:32:44 -0400

    dls class

---
+++ setup.py
@@ -0,0 +1,12 @@
+import sys
+from distutils.core import setup
+
+setup(
+    name="Scritpiboiz from Rob and Jacob",
+    version="0.1",
+    description="Oh yeah",
+    author="Jacob Brady and Rob Harkness",
+    packages=["bradness"],
+    #scripts=files,
+    #package_data={"nmrsa": ["templates/*"]},
+)
